31f216e84b5315918ccb14be702ed50a3f076100
岩本果純 <kpe.359@icloud.com>
2016-03-14 10:43:47 +0900

    Initial Commit

---
+++ Swimee314/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swimee314
+//
+//  Created by 岩本果純 on 2016/03/14.
+//  Copyright © 2016年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
