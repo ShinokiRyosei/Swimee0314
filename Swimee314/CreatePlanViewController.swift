df8ac0059be13c4bf1afdeeba1e5c58773db4286
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-03-14 23:31:19 +0900

    Create CreatePlanViewController Add UITextField method and outlet

---
+++ Swimee314/CreatePlanViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreatePlanViewController.swift
+//  Swimee314
+//
+//  Created by ShinokiRyosei on 2016/03/14.
+//  Copyright © 2016年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class CreatePlanViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var projectTextField: UITextField!
+    @IBOutlet weak var daysTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        projectTextField.delegate = self
+        daysTextField.delegate = self
+        
+        projectTextField.resignFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+
+
+}
