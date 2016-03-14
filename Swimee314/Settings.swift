cd1de15b5da65620cf52f7686138110acf4e8ac5
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-03-14 19:00:49 +0900

    Add Settings.swift and add Colors class

---
+++ Swimee314/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Swimee314
+//
+//  Created by ShinokiRyosei on 2016/03/14.
+//  Copyright © 2016年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+//色を司るクラス
+class Colors {
+    
+}
