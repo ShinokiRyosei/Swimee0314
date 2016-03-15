a539e002e92fc438b7114fd1bef01cdcba1dced3
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-03-16 05:09:35 +0900

    MapKitで検索した地点にピンをたてる

--- Swimee314/LocationViewController.swift
+++ Swimee314/LocationViewController.swift
@@ -13,13 +13,14 @@ class LocationViewController: UIViewController, UISearchBarDelegate, MKMapViewDe
     
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var map: MKMapView!
+    
+    var location: CLLocationCoordinate2D! = CLLocationCoordinate2D()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchBar.delegate = self
         map.delegate = self
-
         // Do any additional setup after loading the view.
     }
 
@@ -29,7 +30,31 @@ class LocationViewController: UIViewController, UISearchBarDelegate, MKMapViewDe
     }
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
         
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(searchBar.text!) { (placemarks, error) -> Void in
+            if let placemark = placemarks?[0] {
+                self.location = CLLocationCoordinate2D(latitude: (placemark.location?.coordinate.latitude)!, longitude: (placemark.location?.coordinate.longitude)!)
+                self.map.setCenterCoordinate(self.location, animated: true)
+//                self.setMapSettings(self.location)
+                self.map.addAnnotation(MKPlacemark(placemark: placemark))
+
+            }
+        }
+        
+    }
+    
+    func setMapSettings(point: CLLocationCoordinate2D) {
+        let leastCoordSpan: Double = 0.005
+        let center = CLLocationCoordinate2DMake(point.latitude, point.latitude)
+        // 縮尺.
+        let latitudeDist : CLLocationDistance = 100
+        let lontitudeDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(center, latitudeDist, lontitudeDist)
+        map.region = region
     }
     
     
--- Swimee314/TravelLocationModel.swift
+++ Swimee314/TravelLocationModel.swift
@@ -5,37 +5,37 @@
 //  Created by ShinokiRyosei on 2016/03/16.
 //  Copyright © 2016年 KasumiIwamoto. All rights reserved.
 //
-
-import UIKit
-import Parse
-
-class TravelLocationModel: PFObject, PFSubclassing {
-    
-    init() {
-        super.init()
-    }
-    
-    override init() {
-        super.init()
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override class func initialize() {
-        struct Static {
-            static var onceToken: dispatch_once_t = 0
-        }
-        
-        dispatch_once(&Static.onceToken) {
-            self.registerSubclass()
-        }
-    }
-    
-    static func parseClassName() -> String {
-        return "Locations"
-    }
-    
-
-}
+//
+//import UIKit
+//import Parse
+//
+//class TravelLocationModel: PFObject, PFSubclassing {
+//    
+//    init() {
+//        super.init()
+//    }
+//    
+//    override init() {
+//        super.init()
+//    }
+//    
+//    required init(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override class func initialize() {
+//        struct Static {
+//            static var onceToken: dispatch_once_t = 0
+//        }
+//        
+//        dispatch_once(&Static.onceToken) {
+//            self.registerSubclass()
+//        }
+//    }
+//    
+//    static func parseClassName() -> String {
+//        return "Locations"
+//    }
+//    
+//
+//}
