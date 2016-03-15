35f02949639f6a8a88c34c29afc5d2fb3aac9d1e
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-03-15 17:21:38 +0900

    Create ScheduleListTableViewCell

---
+++ Swimee314/ScheduleListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleListTableViewCell.swift
+//  Swimee314
+//
+//  Created by ShinokiRyosei on 2016/03/15.
+//  Copyright © 2016年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Swimee314/ScheduleListViewController.swift
+++ Swimee314/ScheduleListViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ScheduleListViewController: UITableViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
